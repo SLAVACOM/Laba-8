0a3c0edefad5711d1175d0cb58f8ecbed4fbdd86
slava <slava@SLAVACOM>
2024-06-03 04:13:12 +0300

    f

--- functions.cpp
+++ functions.cpp
@@ -3,8 +3,9 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
-bool loadDataFromFile(std::list<Person>& lst, const std::string& filename) {
+bool loadDataFromFile(std::list<Person>& people, const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Could not open the file: " << filename << '\n';
@@ -13,48 +14,61 @@ bool loadDataFromFile(std::list<Person>& lst, const std::string& filename) {
 
     std::string line;
     while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        Person person;
-	    std::getline(iss, person.name, ';');			
-        iss >> person.age;
-        iss.ignore(1, ';');
-		std::getline(iss, person.status, ';');
-        lst.push_back(person);
-    }
+        try { 
+            std::istringstream iss(line);
+            Person person;
+            std::string age;
+	        std::getline(iss, person.name, ';');			
+            std::getline(iss, age, ';');
+	    	std::getline(iss, person.status, ';');
+            if (!person.name.empty() && validAge(age) && !person.status.empty()) {
+                person.age = std::stoi(age);
+                people.push_back(person);
+            }else if (line.empty()) return true;
+            else return false;
+	    }
+	    catch (const std::exception& e) {
+		    std::cerr << "Данные неверные в файле" << std::endl;
+		    return false;
+	    }
+        }
 
     file.close();
 	return true;
 }
-
-void printList(const std::list<Person>& lst) {
-    for (const auto& person : lst) {
+bool validAge(std::string age) {
+	std::regex valid_age("[1-9][0-9]?[0-9]?|0");
+	return regex_match(age, valid_age);
+}
+void printList(const std::list<Person>& people) {
+    for (const auto& person : people) {
         std::cout << "Name: " << person.name << ", Age: " << person.age << ", Status: " << person.status << std::endl;
     }
 }
 
 
-bool saveToFile(const std::list<Person>& lst, const std::string& filename) {
+bool saveToFile(const std::list<Person>& people, const std::string& filename) {
     std::ofstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Could not open the file: " << filename << std::endl;
         return false;
     }
-    for (const auto& person : lst) {
-        file << person.name << "; " << person.age << "; " << person.status << std::endl;
+    for (const auto& person : people) {
+        file << person.name << ";" << person.age << ";" << person.status << std::endl;
     }
     
     file.close();
     return true;
 }
 
-std::list<Person>::iterator findElementByName(std::list<Person>& lst, const std::string& name) {
-    return std::find_if(lst.begin(), lst.end(), [&name](const Person& person) {
+std::list<Person>::iterator findElementByName(std::list<Person>& people, const std::string& name) {
+    return std::find_if(people.begin(), people.end(), [&name](const Person& person) {
         return person.name == name;
     });
 }
 
-void resetAllElements(std::list<Person>& lst) {
-    for (auto& person : lst) {
+void resetAllElements(std::list<Person>& people) {
+    for (auto& person : people) {
         person.name = constants::DEFAULT_NAME;
         person.age = constants::DEFAULT_AGE;
         person.status = constants::DEFAULT_STATUS;
@@ -67,36 +81,36 @@ void printFunctionsList() {
 	std::cout << "4)Exit" << std::endl;
 }
 
-void sortByParam(std::list<Person>& lst, int param){
+void sortByParam(std::list<Person>& people, int param){
 
     switch (param)    {
     case 1:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.name < b.name;
 	    });
         break;
     case 2:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.name > b.name;
 	    });
         break;
     case 3:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.age < b.age;
 	    });
         break;
     case 4:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.age > b.age;
 	    });
         break;    
     case 5:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.status < b.status;
 	    });
         break;
     case 6:
-        lst.sort([](const Person& a, const Person& b) {
+        people.sort([](const Person& a, const Person& b) {
 		    return a.status > b.status;
 	    });
         break;   	
--- functions.h
+++ functions.h
@@ -8,17 +8,17 @@ struct Person {
     int age;
     std::string status;
 };
-
-void printList(const std::list<Person>& lst);
+bool validAge(std::string age);
+void printList(const std::list<Person>& people);
 void printFunctionsList();
 
-bool loadDataFromFile(std::list<Person>& lst, const std::string& filename);
-bool saveToFile(const std::list<Person>& lst, const std::string& filename);
+bool loadDataFromFile(std::list<Person>& people, const std::string& filename);
+bool saveToFile(const std::list<Person>& people, const std::string& filename);
 
-std::list<Person>::iterator findElementByName(std::list<Person>& lst, const std::string& name);
+std::list<Person>::iterator findElementByName(std::list<Person>& people, const std::string& name);
 
-void resetAllElements(std::list<Person>& lst);
+void resetAllElements(std::list<Person>& people);
 
-void sortByParam(std::list<Person>& lst, int param);
+void sortByParam(std::list<Person>& people, int param);
 
 
